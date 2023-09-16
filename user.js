9d8011f41ca858acf943a760e85934faed456974
Luminar Leaf <80571430+LuminarLeaf@users.noreply.github.com>
2023-09-16 21:13:47 +0530

    yes

---
+++ user.js
@@ -0,0 +1,17 @@
+// enable smooth scrolling
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 600);
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 750);
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 500);
+user_pref("mousewheel.acceleration.factor", 6);
+user_pref("mousewheel.acceleration.start", 6);
+
+// enable userChrome.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Open PDFs in browser by default
+user_pref("browser.download.open_pdf_attachments_inline", true);
+
+// Disable Pocket
+user_pref("extensions.pocket.enabled", false);
+
